fb277ac515176a772c494c6d95f29c91e13c6332
zhaose233 <zhaose233@outlook.com>
2025-02-16 06:01:47 +0800

    ollama fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone "https://gitlab.com/zhaose233/ollama-rocm-package.git"
+git clone "https://gitlab.com/zhaose233/ollama-rocm-package.git" -b rocm-gtt
 cd "$1"
 makepkg -sf --noconfirm --skippgpcheck
